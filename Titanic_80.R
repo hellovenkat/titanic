b4b445ccfca0101bcc3ed39407befb08864d3db4
Venkat Kotha <venkataratnamkumar7777@gmail.com>
2017-07-13 00:07:12 -0400

    just a dummy change

--- Titanic_80.R
+++ Titanic_80.R
@@ -2,7 +2,6 @@ train_data <- read.csv('/home/venkat/Desktop/train.csv')
 test_data <- read.csv('/home/venkat/Desktop/test.csv')
 dim(train_data)
 dim(test_data)
-dim(test_data)
 train_data$Cabin <- NULL
 test_data$Cabin <- NULL
 train_data$Ticket <- NULL
